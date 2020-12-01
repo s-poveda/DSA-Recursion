284b180f67bc6e08a2204236fd90d60a00a2b950
sam p <spoveda545@gmail.com>
2020-12-01 16:00:59 -0500

    updated

--- factorial.js
+++ factorial.js
@@ -1,4 +1,10 @@
-console.log((function factorial(num) {
+function factorial(num) {
+	if (num < 0) throw new Error('Number must be greater than 0');
+	if (num === 1 || num === 0) return num;
 	if (num === 2) return 2;
 	return factorial(num - 1) * num;
-})(5)); // 120
+}
+
+console.log(factorial(4));
+
+module.exports = factorial;
