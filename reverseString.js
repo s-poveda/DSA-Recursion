f565978944891cdf30f11ac0e3d864b322195f40
sam p <spoveda545@gmail.com>
2020-12-04 11:01:26 -0500

    done orgChart

--- reverseString.js
+++ reverseString.js
@@ -1,4 +1,4 @@
 console.log((function reverseString(str) {
-	if (str.length === 0) return '';
+	if (str.length <= 1) return str;
 	return str.substr(-1) + reverseString(str.substr(0, str.length -1));
 })('hello'));
