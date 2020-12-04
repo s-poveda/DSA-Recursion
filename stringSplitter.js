90cc6859f81e98fbc7a5826d66a4a49968a2f15a
sam p <spoveda545@gmail.com>
2020-12-04 11:17:50 -0500

    wip

--- stringSplitter.js
+++ stringSplitter.js
@@ -1,4 +1,8 @@
-console.log((function stringSplitter(str, char, charInd){
-
+console.log((function stringSplitter(str, char, results = []){
+	if (!str.length) return results;
+	results.push(str.substr(0,1));
+	return 	stringSplitter(str.substr(1), char, results)
+	// .filter(c => c !== char);
+// TODO: finish this
 })
-('1/2/3', '/'));
+('abc/2/t', '/'));
