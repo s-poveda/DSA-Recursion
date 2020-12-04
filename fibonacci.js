3af486662c6d913b4aeeee6a38ae4029c9e01d72
sam p <spoveda545@gmail.com>
2020-12-04 12:47:10 -0500

    reworked fibonacci: iteration

--- fibonacci.js
+++ fibonacci.js
@@ -1,6 +1,11 @@
 console.log((function fibonacci(target) {
 	if (target < 0) throw new Error('Buddy... just stop');
-	if (target === 0) return 0;
-	if (target === 1) return 1;
-	return currentTerm = fibonacci(target - 1) + fibonacci(target - 2);
-})(7)) // 13
+	if (target === 0 || target === 1) return target;
+	let last = 1, beforeLast = 0;
+	for (null; target - 2 >= 0; target--) {
+		const sum = last + beforeLast;
+		beforeLast = last;
+		last = sum;
+	}
+	return last;
+})(5)) // 13
